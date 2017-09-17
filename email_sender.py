93359f5ce495cb48e743a8814f29b3bc7ebc3d7f
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-09-17 02:45:24 -0300

    Add files via upload

---
+++ email_sender.py
@@ -0,0 +1,55 @@
+import os
+
+import smtplib
+
+from datetime import datetime
+
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
+
+
+def get_newest_file(path):
+    files = os.listdir(path)
+    paths = [os.path.join(path, basename) for basename in files]
+    return max(paths, key=os.path.getctime)
+
+
+def send_email():
+    fromaddr = 'sender@email.com'
+    password = ''
+    toaddr = 'recipient@email.com'
+
+    message = MIMEMultipart()
+
+    message['From'] = fromaddr
+    message['To'] = toaddr
+    message['Subject'] = 'Face detected'
+
+    now = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+
+    body = 'This face was detected on camera at {}.'.format(now)
+
+    message.attach(MIMEText(body, 'plain'))
+
+    attachment = open(get_newest_file('history'), "rb")
+    filename = 'face_detected.jpg'
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    message.attach(part)
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, password)
+    text = message.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+
+
+if __name__ == "__main__":
+    pass
---
+++ face_detector.py
@@ -0,0 +1,78 @@
+import os
+import cv2
+import copy
+import numpy as np
+
+import shutil
+import time
+from datetime import datetime
+
+from multiprocessing import Process
+
+from email_sender import send_email
+
+
+def run(instant, frequency):
+    while True:
+        ret, frame = capture.read()
+        image = copy.deepcopy(frame)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        captured_face = None
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            captured_face = image
+
+        cv2.imshow('Face detector', frame)
+
+        if captured_face is not None:
+            instant = send_notification(captured_face, instant, frequency)
+            captured_face = None
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+def send_notification(frame, instant, frequency):
+    if instant < 0 or time.time() - instant >= 60 * frequency:
+
+        instant = time.time()
+
+        if not os.path.exists('history'):
+            os.makedirs('history')
+
+        filename = 'history/{}.jpg'.format(
+            datetime.now().strftime('%d-%m-%Y_%H-%M-%S'))
+
+        cv2.imwrite(filename, frame)
+
+        process = Process(target=send_email)
+        process.start()
+
+        return instant
+    return instant
+
+
+if __name__ == '__main__':
+    shutil.rmtree('history')
+
+    frequency = 3  # minutes
+    instant = -1
+
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    capture = cv2.VideoCapture(0)
+
+    run(instant, frequency)
